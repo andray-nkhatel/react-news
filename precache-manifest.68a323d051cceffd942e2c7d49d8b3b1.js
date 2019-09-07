4e73bee0f21258a6affff2e335b82d5ce408f1b5
andray-nkhatel <andreankhata@gmail.com>
2019-09-07 22:59:27 +0200

    Updates

---
+++ precache-manifest.68a323d051cceffd942e2c7d49d8b3b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e7df3d00c2ce867344b8d1742d495a",
+    "url": "/react-news/index.html"
+  },
+  {
+    "revision": "6446925fad4a2ee2aed2",
+    "url": "/react-news/static/css/main.4ac5fce9.chunk.css"
+  },
+  {
+    "revision": "5a4006d300f31c86940e",
+    "url": "/react-news/static/js/2.a410a479.chunk.js"
+  },
+  {
+    "revision": "6446925fad4a2ee2aed2",
+    "url": "/react-news/static/js/main.9895a14e.chunk.js"
+  },
+  {
+    "revision": "5c7e382a1a7e49f9a9e5",
+    "url": "/react-news/static/js/runtime~main.1b53e7d0.js"
+  }
+]);
